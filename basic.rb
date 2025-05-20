1bda7cc69f97d6e4bc2972e023d861edd068505d
H3BCKN <ka.koszewski@gmail.com>
2025-05-20 21:15:02 +0200

    clear screen command

--- basic.rb
+++ basic.rb
@@ -1,6 +1,7 @@
-# Interactive shell - the entry point to our interpreter
-require 'readline'
+# Global variables at t
+$buffer = {}  # Stores the program lines
 
+# Enhanced main function with program management
 def main
   puts "Ruby Altair Basic 0.0.1, 1975-#{Time.now.year}"
   puts "Type 'HELP' for available commands"
@@ -8,26 +9,52 @@ def main
   loop do
     line = Readline.readline('> ', true)
     
-    # add non empty lines to history 
     if !line.empty? && (Readline::HISTORY.length == 0 || Readline::HISTORY[-1] != line)
       Readline::HISTORY.push(line) 
-    end 
+    end
 
     case line
     when 'QUIT'
       break
-    when 'CLEAR'
+    when 'NEW'
+      $buffer = {}
+    when 'LIST'
+      if $buffer.empty?
+        puts 'Program is empty'
+      else
+        $buffer.keys.sort.each do |num|
+          puts "#{num} #{$buffer[num]}"
+        end
+      end
+     when 'CLEAR'
       system('clear') if RUBY_PLATFORM =~ /linux|bsd|darwin/
       system('cls') if RUBY_PLATFORM =~ /mswin|mingw|cygwin/
     when 'HELP'
       puts "\nAvailable commands:"
       puts '  RUN      - Run the program (not implemented yet)'
-      puts '  NEW      - Clear the program (not implemented yet)'
-      puts '  LIST     - List the program (not implemented yet)'
+      puts '  NEW      - Clear the program'
+      puts '  LIST     - List the program'
       puts '  CLEAR    - Clear the screen'
       puts '  QUIT     - Exit the interpreter'
     else
-      puts "> #{line}"
+      begin
+        unless line.strip.empty?  # Skip empty lines
+          parts = line.split(' ', 2)
+          if parts[0].match?(/^\d+$/)  # Line starts with a number
+            line_num = parts[0].to_i
+            if parts.length > 1
+              $buffer[line_num] = parts[1]  # Store the line
+            elsif $buffer.key?(line_num)
+              $buffer.delete(line_num)  # Remove the line if only number given
+            end
+          else
+            puts "> #{line}"
+          end
+        end
+      rescue Interrupt
+        puts "\nInterrupted. Type 'QUIT' to exit."
+      rescue StandardError => e
+        puts "Unexpected error: #{e}"
     end
   end
 end
